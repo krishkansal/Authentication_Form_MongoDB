e51d5d44b0a2bca8bfccf14123135b1686cebe19
Krish Kansal <110353778+krishkansal@users.noreply.github.com>
2023-04-13 02:31:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const auth = require("./middleware/auth");
 require("./DB/conn");
 const data=require("./model/signup")
 const app=express()
-const port=3000 || process.env.PORT
+const port=process.env.PORT
 
 
 app.use(express.json());
